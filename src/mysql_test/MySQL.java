5de13a6481513ae023993fb9758632815f6584a2
Antony Garcia <antony.garcia.gonzalez@gmail.com>
2015-01-02 22:08:48 -0500

    Cambios realizados hasta al noche del 2 de enero de 2015

--- src/mysql_test/MySQL.java
+++ src/mysql_test/MySQL.java
@@ -1,15 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package mysql_test;
 
-/**
- *
- * @author Antony
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class MySQL {
-    
+
+    private static Connection Conexion;
+
+    public void MySQLConnection(String user, String pass, String db_name) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306" + db_name, user, pass);
+            JOptionPane.showMessageDialog(null, "Se ha iniciado la conexión con el servidor de forma exitosa");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            Conexion.close();
+            JOptionPane.showMessageDialog(null, "Se ha finalizado la conexión con el servidor");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void createDB(String name) {
+        try {
+            String Query = "CREATE DATABASE " + name;
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+            closeConnection();
+            MySQLConnection("root", "", name);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void createTable(String name) {
+        try {
+            String Query = "CREATE TABLE " + name + "(Nombre VARCHAR(50), APELLIDO VARCHAR(50), Edad VARCHAR(3), Sexo VARCHAR(1))";
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
